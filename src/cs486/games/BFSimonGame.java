946bce4f96f70ff13affd58ccfc2211e8962ee66
Biff <jemonono@BIFFTHESUPERB-VirtualBox.(none)>
2012-05-02 10:15:48 -0500

    Game should be functionally complete. Might want to consider adding a score display and some on screen instructions,  but as of right now this should otherwise be perfectly functional. Be aware that some of your faster clicks may not be recognized.

--- src/cs486/games/BFSimonGame.java
+++ src/cs486/games/BFSimonGame.java
@@ -2,18 +2,23 @@ package cs486.games;
 
 import java.awt.Color;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import cs486.managers.BFManager;
 
 public class BFSimonGame extends BFGame implements java.awt.event.ActionListener, Runnable{
 
 	protected static Color[] buttonColors = {Color.RED, Color.BLUE , Color.GREEN, Color.YELLOW};
+	//java.util.LinkedList<Integer> sequence;
 	java.util.LinkedList<Integer> sequence;
 	
 	long startTime;
 	int guessIndex; //the index of the sequence which needs to be guessed next.
 	boolean playback;//indicates the game is playing back a 
-	java.util.LinkedList<javax.swing.JButton> buttons;
+	//java.util.LinkedList<javax.swing.JButton> buttons;
+	java.util.LinkedList<SimonButton> buttons;
+	
 	
 	
 	
@@ -25,56 +30,79 @@ public class BFSimonGame extends BFGame implements java.awt.event.ActionListener
 	}
 
 	protected void start(){
-		this.setBackground(Color.WHITE);
-		repaint();
+		
 		sequence = new java.util.LinkedList<Integer>();
 		startTime = System.currentTimeMillis();
 		playback = true;
+		initializeDisplay();
+		addAndPlaySequence();
+
+		
+	}
+
+	private void initializeDisplay() {
+		this.setBackground(Color.WHITE);
+		repaint();
 		if(buttons == null){
-			buttons = new java.util.LinkedList<javax.swing.JButton>();
+			//buttons = new java.util.LinkedList<javax.swing.JButton>();
+			buttons = new java.util.LinkedList<SimonButton>();
 			for(int i = 0; i < 4 ; i++){
-				javax.swing.JButton newbutton = new javax.swing.JButton();
-				
-				add(newbutton);
-				newbutton.setActionCommand(Integer.toString(i));
-				newbutton.addActionListener(this);
-				newbutton.setPreferredSize(new java.awt.Dimension(200, 200));
-				newbutton.setLocation((i % 2) * 400, (i * 2) * 400);
+				//javax.swing.JButton newbutton = new javax.swing.JButton();
+				SimonButton newButton = new SimonButton(this, buttonColors[i]);
+				add(newButton);
+				//newbutton.setActionCommand(Integer.toString(i));
+				//newbutton.addActionListener(this);
+				newButton.setPreferredSize(new java.awt.Dimension(200, 200));
+				newButton.setLocation((i % 2) * 300, (i * 2) * 300);
+			
 				repaint();
 				//newbutton.s
-				newbutton.setBackground(buttonColors[i]);
-				newbutton.setForeground(buttonColors[i]);
-				
+				//newbutton.setBackground(buttonColors[i]);
 			
-				buttons.add(new javax.swing.JButton());
+				buttons.add(newButton);
 				
 			}
+			javax.swing.JButton newbutton= new javax.swing.JButton();
+			newbutton.setActionCommand("Start");
+			newbutton.addActionListener(this);
+			newbutton.setPreferredSize(new java.awt.Dimension(250, 100));
+			newbutton.setText("Start");
+			newbutton.setLocation(450, 240);
+			this.add(newbutton);
+			//buttons.add(newbutton);
 		}
+		
 		repaint(0);
-		playback = true;
-		//this.
-//	    try{
-//	    	Thread.sleep(2500);
-//	    } catch (Exception e){}
 		
 	}
 	
 	private void addAndPlaySequence() {
 		playback = true;
         sequence.add((int) (Math.floor(Math.random() * 4.0) % 4));
+        for(SimonButton s : buttons){
+			s.resetColor();
+		}
+//        System.out.println(sequence.size());
         for(int i = 0; i < sequence.size(); i++){
             int buttonNumber = sequence.get(i);
-            buttons.get(buttonNumber).setBackground(Color.WHITE);
+            buttons.get(buttonNumber).shadeButton();
             repaint(0);
-            try {
-				Thread.sleep(400);
-			} catch (InterruptedException e){}
-            buttons.get(buttonNumber).setBackground(buttonColors[buttonNumber]);
+            this.paintImmediately(getBounds());
+//            try {
+//				Thread.sleep(400);
+//			} catch (InterruptedException e){}
+            for(int x= 0; x < 6000000; x++){
+            	repaint();
+            }
+            buttons.get(buttonNumber).resetColor();
+            this.paintImmediately(getBounds());
+            //buttons.get(buttonNumber).repaint(0);
             repaint();
             try {
             	Thread.sleep(100);
             } catch (InterruptedException e){}
         }
+        //this
         guessIndex = 0;
 		playback = false;
 	}
@@ -93,39 +121,48 @@ public class BFSimonGame extends BFGame implements java.awt.event.ActionListener
 
 	@Override
 	public void reportScore() {
-       if(sequence != null){
+       if(sequence == null){
     	   BFManager.getInstance().newHighScore(0);
+    	   //System.out.println(0);
        } else{
     	   BFManager.getInstance().newHighScore(sequence.size() - 1);
+    	   //System.out.println(sequence.size() - 1);
        }
+	}
+	public void clickButton(SimonButton simonButton, Color buttonColor){
+		//this.setBackground(buttonColor);
+		if(playback || sequence.size()<=0) return;
+		for (int i = 0; i < buttons.size(); i++){
+		if(buttonColor.equals(buttonColors[i])){
+			if(i == sequence.get(guessIndex)){
+				guessIndex++;
+				if(guessIndex >= sequence.size()){
+					addAndPlaySequence();
+				}
+				//continue the current round
+				//as in, do nothing right here, leave the loop
+				return;
+			}else{
+				sequenceMismatch();
+				return;
+			}
+		}
+		}
+	    addAndPlaySequence();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		if(playback || sequence.size()<=0) return;
-		for (int i = 0; i < buttons.size(); i++){
-			if(Integer.toString(i).equals(arg0.getActionCommand())){
-				if(i == sequence.get(guessIndex)){
-					guessIndex++;
-					if(guessIndex >= sequence.size()){
-						addAndPlaySequence();
-					}
-					//continue the current round
-					//as in, do nothing right here, leave the loop
-					return;
-				}else{
-					sequenceMismatch();
-					return;
-				}
-			}
-		}
-		addAndPlaySequence();
+		start();
 	}
 
 	private void sequenceMismatch() {
 		// TODO Auto-generated method stub
 		reportScore();
-		start();
+		for(SimonButton s : buttons){
+			s.setBackground(Color.BLACK);
+		}
+		//start();
 		
 		
 	}
@@ -134,6 +171,64 @@ public class BFSimonGame extends BFGame implements java.awt.event.ActionListener
 	public void run() {
 		// TODO Auto-generated method stub
 		start();
+	}
+	/**
+	 * I can't believe I'm doing this.
+	 * @author jemonono
+	 *
+	 */
+	protected class SimonButton extends javax.swing.JPanel implements MouseListener{
+		public Color buttonColor;
+		BFSimonGame game;
+		
+		public SimonButton(BFSimonGame game, Color color){
+			buttonColor = color;
+			this.addMouseListener(this);
+			this.setBackground(buttonColor);
+		    this.game = game;
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			game.clickButton(this, buttonColor);
+			
+		}
+		
+		public void resetColor(){
+			this.setBackground(buttonColor);
+		}
+		
+		public void shadeButton(){
+			//this.setBackground(buttonColor.brighter());
+			this.setBackground(Color.BLACK);
+		}
+		
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+			//do nothing
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			//do nothing
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			shadeButton();
+			repaint();
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			resetColor();
+			repaint();
+			
+			
+		}
 	}
 
 }
